39d4e2d40a734663b617f7a71496af4cf3a746b1
zepto5 <andre.hukkelaas@abaris.no>
2019-03-22 13:13:32 +0000

    b529cf0 DHIS2-5933

---
+++ static/js/240.6cb50fdb.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([240],{1855:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
---
+++ static/js/253.5dbb47c0.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([253],{1842:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
