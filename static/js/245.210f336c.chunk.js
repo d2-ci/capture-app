17e0e9fc650736d5036d4be7635cea12f5c0b51a
Philip-Larsen-Donnelly <phil@dhis2.org>
2019-09-06 11:28:44 +0000

    d055f85 chore: transifex integration

---
+++ static/js/245.210f336c.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([245],{1983:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
---
+++ static/js/253.cd335816.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([253],{1975:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
