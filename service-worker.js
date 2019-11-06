eeb4a05fa8725ec8ae8a3a4d85e7be303ffcdf42
GitHub <noreply@github.com>
2019-11-06 08:39:23 +0000

    14b8535 fix: DHIS2-7259 performance fixes with large metadata volume

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1c46f039c965849cdd172452a2261d19.js"
+  "./precache-manifest.c064dadbdec806c1d2f4923961fbf715.js"
 );
 
 self.addEventListener('message', (event) => {
