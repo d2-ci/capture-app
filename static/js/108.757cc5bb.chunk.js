aece0c31a6a295ac1e0b469cbabd6b005f586ac8
paschalidi <paschalidi@outlook.com>
2020-04-27 07:18:07 +0000

    2c1dd7b chore: refactors to getEffects being returned

---
+++ static/js/108.757cc5bb.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[108],{1373:function(n){n.exports={typings:"../../typings.d.ts"}}}]);
---
+++ static/js/118.305d50e0.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[118],{1383:function(n){n.exports={typings:"../../typings.d.ts"}}}]);
