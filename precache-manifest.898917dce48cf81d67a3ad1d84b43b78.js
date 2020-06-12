e84bc71f6704e61ee822bf2f75f4bba481f44f0e
Christos Paschalidis <paschalidi@outlook.com>
2020-06-12 14:43:21 +0000

    1bb211b feat: styles

--- precache-manifest.898917dce48cf81d67a3ad1d84b43b78.js
+++ precache-manifest.898917dce48cf81d67a3ad1d84b43b78.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83cf0650b8c929a39cb34e16d36ec641",
+    "revision": "0fa3651cc5d2237bbf8efe4501e651fc",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/256.d11fbfae.chunk.css"
   },
   {
-    "revision": "51cfaeb5603ff73129b2",
+    "revision": "3a1268b1c380a50255c0",
     "url": "./static/css/main.f037c903.chunk.css"
   },
   {
@@ -1036,8 +1036,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/99.f31f2f89.chunk.js"
   },
   {
-    "revision": "51cfaeb5603ff73129b2",
-    "url": "./static/js/main.e034e407.chunk.js"
+    "revision": "3a1268b1c380a50255c0",
+    "url": "./static/js/main.2e0dd16a.chunk.js"
   },
   {
     "revision": "1044988fb673e2f01968",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.54d5b45b0d68c676a18c6896bd957afc.js"
+  "./precache-manifest.898917dce48cf81d67a3ad1d84b43b78.js"
 );
 
 self.addEventListener('message', (event) => {
