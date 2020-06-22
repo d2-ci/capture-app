ab99545bc66e382790ca5ed7d9107bd7aee672aa
paschalidi <paschalidi@outlook.com>
2020-06-22 11:07:59 +0000

    17e35b2 chore: travis lint

--- src/core_modules/capture-core/HOC/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export { default as withTransformPropName } from './withTransformPropName';
-export { default as withLoadingIndicator } from './withLoadingIndicator';
-export { default as withErrorMessageHandler } from './withErrorMessageHandler';
