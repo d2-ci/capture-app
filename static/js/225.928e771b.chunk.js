55a44c4512139ce4bf8f96dd400142ad204342ca
Philip-Larsen-Donnelly <phil@dhis2.org>
2020-07-13 22:35:49 +0000

    aeb81a9 chore(release-prepare): cut patch/2.32.6

---
+++ static/js/225.928e771b.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([225],{2003:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
---
+++ static/js/228.3ceafba3.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([228],{2e3:function(n,p){n.exports={typings:"../../typings.d.ts"}}});
