632083d27e4827d19d417f24841618ffe3d4d970
paschalidi <paschalidi@outlook.com>
2020-09-01 11:35:22 +0000

    5f97443 chore: annotates

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.197157828e83ce31a3821838d0dd98d9.js"
+  "./precache-manifest.677bb844756742f68cea58e8118d38c2.js"
 );
 
 self.addEventListener('message', (event) => {
