f34d645c9cad8b6a930a32f26324b4af5faaa856
GitHub <noreply@github.com>
2020-09-07 17:13:24 +0000

    3546be6 chore(deps-dev): bump eslint from 6.8.0 to 7.8.1

---
+++ src/core_modules/capture-core/components/D2Form/field/configs/base/configBaseCustomForm.js
@@ -0,0 +1,21 @@
+// @flow
+import type { FieldConfig } from 'capture-ui/FormBuilder/FormBuilder.component';
+import { getBaseConfigForField } from './configBase';
+import MetaDataElement from '../../../../../metaData/DataElement/DataElement';
+import type { FieldConfigForType } from './configBase';
+
+const getBaseProps = (metaData: MetaDataElement) => ({
+    metaCompulsory: metaData.compulsory,
+});
+
+export const createProps = (props?: ?Object, metaData: MetaDataElement) => ({
+    ...getBaseProps(metaData),
+    ...props,
+});
+
+// $FlowFixMe[prop-missing] automated comment
+// $FlowFixMe[incompatible-return] automated comment
+export const createFieldConfig = (fieldSpecifications: FieldConfigForType, metaData: MetaDataElement): FieldConfig => ({
+    ...getBaseConfigForField(metaData),
+    ...fieldSpecifications,
+});
