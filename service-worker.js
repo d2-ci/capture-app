4668a957b0d6328c4fafbb6a3dee9cfa419e0144
paschalidi <paschalidi@outlook.com>
2020-09-09 05:52:42 +0000

    bed428d Merge branch 'cp/search/04-DHIS2-8927-search-by-attribute-search-groups' into cp/search/05-DHIS2-8923-back-functionality-from-8927

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.401eeb5ecc3abbfd3df4980ccf5b4871.js"
+  "./precache-manifest.84c51e5a686c5c54b57a7835585e9c76.js"
 );
 
 self.addEventListener('message', (event) => {
