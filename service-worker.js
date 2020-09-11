089de368b9b3aab8074a68b99e78a33d7e5f6293
paschalidi <paschalidi@outlook.com>
2020-09-11 13:38:49 +0000

    1c2a666 chore: finishes rebasing

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c79567a54703ef4466e0b2704d24332.js"
+  "./precache-manifest.f9f6fb1e79d20e2b3b033ee38cba696d.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/247.5755b0e1.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpcapture-app"]=this["webpackJsonpcapture-app"]||[]).push([[247],{1358:function(p){p.exports=JSON.parse('{"typings":"../typings.d.ts"}')}}]);
