68a373f76a695c421ca9ef47e79737d44a096294
GitHub <noreply@github.com>
2020-09-14 16:51:18 +0000

    1e36f54 chore(deps-dev): bump eslint from 6.8.0 to 7.9.0

---
+++ src/core_modules/capture-core/components/Pages/NewEvent/epics/newEvent.epics.js
@@ -0,0 +1,27 @@
+// @flow
+import { push } from 'connected-react-router';
+import { ofType } from 'redux-observable';
+import { map } from 'rxjs/operators';
+import { lockedSelectorActionTypes } from '../../../LockedSelector/LockedSelector.actions';
+
+const getArguments = (programId: string, orgUnitId: string) => {
+    const argArray = [];
+    if (programId) {
+        argArray.push(`programId=${programId}`);
+    }
+    if (orgUnitId) {
+        argArray.push(`orgUnitId=${orgUnitId}`);
+    }
+
+    return argArray.join('&');
+};
+
+export const openNewEventPageLocationChangeEpic = (action$: InputObservable, store: ReduxStore) =>
+    action$.pipe(
+        ofType(lockedSelectorActionTypes.NEW_EVENT_OPEN),
+        map(() => {
+            const state = store.value;
+            const { programId, orgUnitId } = state.currentSelections;
+            const args = getArguments(programId, orgUnitId);
+            return push(`/newEvent/${args}`);
+        }));
