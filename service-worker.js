add71efd4fb121197a313760274da10efee5d894
GitHub <noreply@github.com>
2020-10-16 12:15:46 +0000

    e334c38 Merge chained pull request #982 TECH-435 DHIS2-9665 DHIS2-9675

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.638b6507097238e84c4a7eff8c8427c9.js"
+  "./precache-manifest.fc3d04f1fec8dea7a6f01b3c20917e66.js"
 );
 
 self.addEventListener('message', (event) => {
