1ccd5d98f6bbb68d3cb16278dc005c5fe52bf40a
GitHub <noreply@github.com>
2020-10-27 07:28:53 +0000

    d66de1c chore(deps-dev): bump jest-localstorage-mock from 2.4.0 to 2.4.3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a2a0240a08b174deb014b54e7f62f18f.js"
+  "./precache-manifest.c8456d7856321bebaa29aa39d6e2a81e.js"
 );
 
 self.addEventListener('message', (event) => {
