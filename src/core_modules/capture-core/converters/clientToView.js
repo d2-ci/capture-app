d84bf37da11b0f4c881904f1e31884cb862f8391
GitHub <noreply@github.com>
2020-11-09 06:38:55 +0000

    34c3729 chore(deps-dev): bump eslint from 6.8.0 to 7.13.0

---
+++ src/core_modules/capture-core/converters/clientToView.js
@@ -0,0 +1,74 @@
+// @flow
+import React from 'react';
+import { moment } from 'capture-core-utils/moment';
+import { dataElementTypes, type DataElement } from '../metaData';
+import { convertMomentToDateFormatString } from '../utils/converters/date';
+import stringifyNumber from './common/stringifyNumber';
+import { MinimalCoordinates } from '../components/MinimalCoordinates';
+
+function convertDateForView(rawValue: string): string {
+    const momentDate = moment(rawValue);
+    return convertMomentToDateFormatString(momentDate);
+}
+
+function convertDateTimeForView(rawValue: string): string {
+    const momentDate = moment(rawValue);
+    const dateString = convertMomentToDateFormatString(momentDate);
+    const timeString = momentDate.format('HH:mm');
+    return `${dateString} ${timeString}`;
+}
+
+function convertTimeForView(rawValue: string): string {
+    const momentDate = moment(rawValue, 'HH:mm', true);
+    return momentDate.format('HH:mm');
+}
+
+type FileClientValue = {
+    name: string,
+    url: string,
+    value: string,
+};
+
+function convertResourceForView(clientValue: FileClientValue) {
+    return (
+        <a
+            href={clientValue.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={(event) => { event.stopPropagation(); }}
+        >
+            {clientValue.name}
+        </a>
+    );
+}
+
+const valueConvertersForType = {
+    [dataElementTypes.NUMBER]: stringifyNumber,
+    [dataElementTypes.INTEGER]: stringifyNumber,
+    [dataElementTypes.INTEGER_POSITIVE]: stringifyNumber,
+    [dataElementTypes.INTEGER_ZERO_OR_POSITIVE]: stringifyNumber,
+    [dataElementTypes.INTEGER_NEGATIVE]: stringifyNumber,
+    [dataElementTypes.DATE]: convertDateForView,
+    [dataElementTypes.DATETIME]: convertDateTimeForView,
+    [dataElementTypes.TIME]: convertTimeForView,
+    [dataElementTypes.TRUE_ONLY]: () => 'Yes',
+    [dataElementTypes.BOOLEAN]: (rawValue: boolean) => (rawValue ? 'Yes' : 'No'),
+    [dataElementTypes.COORDINATE]: MinimalCoordinates,
+    [dataElementTypes.AGE]: convertDateForView,
+    [dataElementTypes.FILE_RESOURCE]: convertResourceForView,
+    [dataElementTypes.IMAGE]: convertResourceForView,
+    [dataElementTypes.ORGANISATION_UNIT]: (rawValue: Object) => rawValue.name,
+};
+
+export function convertValue(value: any, type: $Keys<typeof dataElementTypes>, dataElement?: ?DataElement) {
+    if (!value && value !== 0 && value !== false) {
+        return value;
+    }
+
+    if (dataElement && dataElement.optionSet) {
+        return dataElement.optionSet.getOptionText(value);
+    }
+
+    // $FlowFixMe dataElementTypes flow error
+    return valueConvertersForType[type] ? valueConvertersForType[type](value) : value;
+}
