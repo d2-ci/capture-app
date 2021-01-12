8f21b015a1f5b16e76d52553421cc953e2d8fe2b
GitHub <noreply@github.com>
2021-01-12 12:19:53 +0000

    6612cbb feat: [part 02] DHIS2-9637 adds new button and unifies the urls | extracting the page to a component (#1314)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cd7bfad960d68971c7e8aff90b7c31e0.js"
+  "./precache-manifest.107deadfd2b88024aa13d877d27458f7.js"
 );
 
 self.addEventListener('message', (event) => {
