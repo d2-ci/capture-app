6490fe0cfaf7219724a399ec3fd07d71c0419f0d
paschalidi <paschalidi@outlook.com>
2021-01-12 11:00:26 +0000

    d08ed91 Merge branch 'cp/registration/16-DHIS2-9637-removes-new-root' into cp/registration/17-DHIS2-9637e2e

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.725a001e06bd7a2ad0dc171de2629974.js"
+  "./precache-manifest.107deadfd2b88024aa13d877d27458f7.js"
 );
 
 self.addEventListener('message', (event) => {
