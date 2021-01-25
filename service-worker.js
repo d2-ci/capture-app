f681e5c857fbaa9f5ff08e067b407f4a12fdbc41
GitHub <noreply@github.com>
2021-01-25 13:50:53 +0000

    450fe0a feat: DHIS2-9658 adds duplicates | DHIS2-10179 Block users from seeing a page when they have no access to it (#1369)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f27bb092c27fac2568af979ad8ce6cc3.js"
+  "./precache-manifest.1fd1c735233a0d13b5e5b32c525708d9.js"
 );
 
 self.addEventListener('message', (event) => {
