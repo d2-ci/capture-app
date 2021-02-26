5de2e6978ef61e18d9c518c8c2b6cc0e728af7c9
paschalidi <paschalidi@outlook.com>
2021-02-26 13:53:29 +0000

    7aea357 chore: rebase and splits the shared steps

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.43704c7396be955b411409dd0c71dc23.js"
+  "./precache-manifest.31e0de24e8a4b48a5bc1df175afc7ede.js"
 );
 
 self.addEventListener('message', (event) => {
