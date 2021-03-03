0c90a107dbecc15349dffa05ed56b79240f9be53
paschalidi <paschalidi@outlook.com>
2021-03-03 08:58:02 +0000

    9998ff6 Merge remote-tracking branch 'origin/v36' into cp/adds-new-icon

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.31e0de24e8a4b48a5bc1df175afc7ede.js"
+  "./precache-manifest.52b700f32aee91390372a3ccdb44b2a8.js"
 );
 
 self.addEventListener('message', (event) => {
