6d8359273cf87ed3fb04e9e6a03c8ca0f0adcc78
GitHub <noreply@github.com>
2021-03-18 09:26:30 +0000

    0ea0d8b Merge pull request #1567 from dhis2/DHIS2-10715-v36

--- precache-manifest.1934b1437464265ab7000f12fd38a44c.js
+++ precache-manifest.1934b1437464265ab7000f12fd38a44c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e418ce1e089429927a07d5dab0d0744f",
+    "revision": "74c1619427a6a28c1a4ac71e14d7e5c4",
     "url": "./index.html"
   },
   {
@@ -12,7 +12,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/278.e705a9da.chunk.css"
   },
   {
-    "revision": "3d293d409a4f51685aa7",
+    "revision": "6b24d950cb245f3fe44d",
     "url": "./static/css/app.0841e961.chunk.css"
   },
   {
@@ -1128,16 +1128,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/99.90bebeb0.chunk.js"
   },
   {
-    "revision": "3d293d409a4f51685aa7",
-    "url": "./static/js/app.aa078cc3.chunk.js"
+    "revision": "6b24d950cb245f3fe44d",
+    "url": "./static/js/app.ec1a1a1e.chunk.js"
   },
   {
     "revision": "f65dce9d1e5f3e1df5c6",
     "url": "./static/js/main.d7f8252e.chunk.js"
   },
   {
-    "revision": "fa2c1b5a77f31362aa7f",
-    "url": "./static/js/runtime-main.b37ab31d.js"
+    "revision": "3b03414dc064797881cf",
+    "url": "./static/js/runtime-main.70688009.js"
   },
   {
     "revision": "0225ccfbe4c6004cb0fec32523bf7427",
