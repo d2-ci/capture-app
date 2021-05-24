c19a3d32fdf88838428c8b81348b39756c847267
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-05-24 07:39:58 +0000

    12bea0f fix: rxjs/operators import

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5a926a46c3b17eb1dc13a55fd889ae7d.js"
+  "./precache-manifest.c437fd24f446fd98d22a40abcd47a4cf.js"
 );
 
 self.addEventListener('message', (event) => {
