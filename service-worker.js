3bf26af9f07ff62329fa88a22700979c1880e5f3
huocha <huongtranguyen.ndht@gmail.com>
2021-07-05 05:03:26 +0000

    e633390 Merge branch 'master' into TECH-612

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.04ef2407dfa3341d82518ce94b2aaab8.js"
+  "./precache-manifest.6ae3206cd863a13bb83f3e0304adb7f9.js"
 );
 
 self.addEventListener('message', (event) => {
