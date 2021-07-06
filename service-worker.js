dcd5d72fcbe814b2f04e2c4b13fd698807843a76
GitHub <noreply@github.com>
2021-07-06 09:22:40 +0000

    91ae330 chore(deps): bump rxjs from 7.1.0 to 7.2.0 (#1871)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.eef42fc4d9dddc34b49d11fadf3945ee.js"
+  "./precache-manifest.e1e641d4b49b144dbdbe73073ca7e2dc.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/111.e956a4c3.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[111],{1369:function(s){s.exports=JSON.parse('{"typings":"../../typings.d.ts"}')}}]);
