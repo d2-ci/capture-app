3686f0741e07f31743ddd80aeeb05ef1d2e87f43
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-08-06 07:49:38 +0000

    b3ad763 Merge branch 'master' into TECH-633

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d8e8cc42f3413ef8be68542dafc6b5e3.js"
+  "./precache-manifest.154c167f45630bbb12ba879852fa1a3c.js"
 );
 
 self.addEventListener('message', (event) => {
