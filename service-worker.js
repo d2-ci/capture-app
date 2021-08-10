c46a9db022709879207cbe77333440de7571ecb2
huocha <huongtranguyen.ndht@gmail.com>
2021-08-10 06:08:07 +0000

    2c7550f Merge branch 'master' into DHIS2-11393

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.27130c7703a64fa6c13baae636116730.js"
+  "./precache-manifest.08686706afd8d9a7b2e42c2690039ee1.js"
 );
 
 self.addEventListener('message', (event) => {
