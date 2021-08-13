f851ef7b50cf9aaacecff7b570449955b7bc51c8
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-08-13 12:39:35 +0000

    ad22cb6 Merge branch 'master' into dependabot/npm_and_yarn/master/react-leaflet-draw-0.19.8

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.70b44649a6f5c0401792d9de084a8a05.js"
+  "./precache-manifest.82fca85c51e9f1fb7a6f93543ff30c2a.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/115.7b551a15.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[115],{1394:function(s){s.exports=JSON.parse('{"typings":"../../typings.d.ts"}')}}]);
