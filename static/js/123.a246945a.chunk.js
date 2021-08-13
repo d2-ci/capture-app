3a84fe243dc930302b3e45031b0c07d35c86b6d0
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-08-13 12:25:45 +0000

    592a9b6 fix: missing dependency

---
+++ static/js/123.a246945a.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[123],{1402:function(s){s.exports=JSON.parse('{"typings":"../../typings.d.ts"}')}}]);
---
+++ static/js/130.a4597134.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonp@dhis2/app-shell"]=this["webpackJsonp@dhis2/app-shell"]||[]).push([[130],{1409:function(s){s.exports=JSON.parse('{"typings":"../../typings.d.ts"}')}}]);
