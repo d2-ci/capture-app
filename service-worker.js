2c9fac590508e9eb5e33df66a321de0f49220df7
jasminenguyennn <jasmine@dhis2.org>
2021-09-07 10:23:06 +0000

    cdc2a97 chore: [DHIS2-11616] update action naming

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.630a186d0e5f08df0de10433ad214d8e.js"
+  "./precache-manifest.1a8863e8f18d91524175da2c579872a0.js"
 );
 
 self.addEventListener('message', (event) => {
