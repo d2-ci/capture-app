23a9f503f945bcc9af3bdfd8763016c6a4134fa8
GitHub <noreply@github.com>
2021-10-21 08:02:59 +0000

    6c50199 fix: [DHIS2-11986] use react-router location in new enrollment event page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e6067c23b7d98dd6907b15035cb03f6f.js"
+  "./precache-manifest.b357a7a19a4d2110e41f943131522741.js"
 );
 
 self.addEventListener('message', (event) => {
